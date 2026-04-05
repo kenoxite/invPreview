7064c8d87221a4ddb075bf0b0d4363438d30a8c2
kenoxite <kenoxite@gmail.com>
2026-04-05 15:04:42 +0200

    images

---
+++ mod.cpp
@@ -0,0 +1,20 @@
+name = "Inventory Preview"; // Name of your mod
+picture = "picture.paa"; // Picture displayed from the expansions menu/ Optimal size is 2048x1024, other sizes work too
+logoSmall = "logo_small.paa"; // Display next to the item added by the mod
+logo = "logo.paa"; // Logo displayed in the main menu
+logoOver = "logo_over.paa"; // When the mouse is over, in the main menu
+action = "https://steamcommunity.com/sharedfiles/filedetails/?id=3036516333"; // Website URL, that can accessed from the expansions menu
+tooltipOwned = "Visit Inventory Preview's workshop page!"; // Tool tip displayed when the mouse is left over, in the main menu
+
+// Color used for DLC stripes and backgrounds (RGBA)
+dlcColor[] =
+{
+	0.23,
+	0.39,
+	0.30,
+	1
+};
+
+overview = "Shows a real-time 3D model of your character in the inventory screen."; // Overview text, displayed from the extension menu
+hideName = 0; // Hide the extension name
+hidePicture = 0; // Hide the extension menu
